bb1cf2eb68a4e4307539cbe8b5f8c45903a82491
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-13 19:52:42 +0530

    first commit

--- controller/pm/update.task.controller.js
+++ controller/pm/update.task.controller.js
@@ -26,7 +26,7 @@ module.exports = async(req, res)=>{
         }
         else{
             Task.findByIdAndUpdate(req.body.id,
-                {$set: req.body,last_update: Date.now()},
+                {$set: req.body,last_update: Date.now(),doc:''},
                 {runValidators:true,upsert:true,new:true},async(er,dt)=>{
                 if(er) return res.status(400).send({type:'error',message: er.message});
                 return res.status(200).send({type:'success',message:'Task updated'});
