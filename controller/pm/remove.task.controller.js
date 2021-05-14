b0e406d0706754c77e0d9c271fcf9befcdd91131
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-15 00:40:18 +0530

    first commit

--- controller/pm/remove.task.controller.js
+++ controller/pm/remove.task.controller.js
@@ -2,74 +2,34 @@ const Task = require('../../models/projectTaskModel');
 const assignTask = require('../../models/assignTaskModel');
 const Team = require('../../models/projectTeamModel');
 
-module.exports = async(req, res)=>{
-    try {
-        Task.findByIdAndDelete(req.body.id,async(er,dt)=>{
-            if(er) return res.status(400).send({type:'error',message: er.message});
-            else{
-                await assignTask.findOneAndDelete({taskRef: req.body.id})
-                .exec(async(err,data)=>{
-                    if(err) return res.status(400).send({type:'error',message: err.message});
-                    // return res.status(200).send({type:'success',message:'Task Deleted'});
-                    else{
-                        const team = await Team.findOne({teamMembers:{$elemMatch:{devRef : data.devRef}}})
-                        .exec((e,d)=>{
-                            if(e) res.status(400).send({tyep: 'error', message: e.message})
-                            else{
-                                let teammembers = d.teamMembers
-                                teammembers = teammembers.map(dev=>({
-                                    devRef: dev.devRef,
-                                    _id: dev._id,
-                                    isAssigned: dev.devRef == data.devRef ? false : dev.isAssigned
-                                }))
-                                Team.findByIdAndUpdate(d._id,{$set: {teamMembers: teammembers}},{new:true},async(er,dt)=>{
-                                    if(er) res.send({type:'error',message:er.message})
-                                    else res.status(200).send({type:'success', message:'Task deleted'})
-                                })
-                            }
-                        })
+module.exports = async (req, res) => {
+    const id = req.body.id;
+    const projectRef = req.body.projectRef
+    await assignTask.find({taskRef: id})
+    .then(async (data) => {
+        if(data.length === 0) {
+            Task.findByIdAndDelete(id).then(data1 => res.send(data1)).catch(error => res.send(error))
+            } 
+            else {
+                const devRef = data[0].devRef
+                const teamData = await Team.findOne({projectRef: projectRef})
+                let team = teamData.teamMembers.map(member => { 
+                    return ({
+                        isAssigned: JSON.stringify(devRef) == JSON.stringify(member.devRef) ? false : member.isAssigned,
+                        _id: member._id,
+                        devRef: member.devRef
+                    })
+                })
+                await Team.findByIdAndUpdate(teamData._id,{$set: {teamMembers: team}},{new:true},async(er,dt)=>{
+                    if(er) res.send({type:'error',message:er.message})
+                    else {
+                        Task.findByIdAndDelete(id)
+                        .then(data1 => {
+                            assignTask.findOneAndDelete({devRef: devRef})
+                            .then(() => res.status(200).send({type:'success', message:'Task deleted'}))
+                        }).catch(error => res.status(400).send({type:'error', message: error.message}))
                     }
-
                 })
             }
         })
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({type: 'error', message: 'Error while connecting to the server!'});
     }
-}
-
-
-// const assignTask = require('../../models/assignTaskModel');
-
-// module.exports = async(req, res) =>{
-//     try {
-//         const {taskRef} = req.body;
-//         await assignTask.findOneAndDelete(taskRef,
-//             async (err, data)=>{
-//                 if(err) return res.status(400).send({type:'error', message: err.message});
-//                 else{
-//                     const team = await Team.findOne({teamMembers:{$elemMatch:{devRef : req.body.devRef}}})
-//                     .exec((e,d)=>{
-//                         if(e) res.status(400).send({tyep: 'error', message: e.message})
-//                         else{
-//                             let teammembers = d.teamMembers
-//                             teammembers = teammembers.map(dev=>({
-//                                 devRef: dev.devRef,
-//                                 _id: dev._id,
-//                                 isAssigned: dev.devRef == req.body.devRef ? false : dev.isAssigned
-//                             }))
-//                             Team.findByIdAndUpdate(d._id,{$set: {teamMembers: teammembers}},{new:true},async(er,dt)=>{
-//                                 if(er) res.send({type:'error',message:er.message})
-//                                 else res.status(200).send({type:'success', message:'Task unassigned'})
-//                             })
-//                         }
-//                     })
-//                 }
-//             })
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send({type: 'error', message: 'Error while connecting to the server!'});
-//     }
-  
-// }
