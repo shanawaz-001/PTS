ad49e7933544efda1e291f3c41139845a1c5601c
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-16 09:53:13 +0530

    first commit

--- controller/pm/add.team.controller.js
+++ controller/pm/add.team.controller.js
@@ -3,11 +3,8 @@ const Team = require('../../models/projectTeamModel');
 module.exports = async(req,res)=>{
     const { projectRef, teamLeader, teamMembers} = req.body;
     try {
-        Team.create({
-            projectRef,
-            teamLeader,
-            teamMembers
-        },async(err,dt)=>{
+        Team.findOneAndUpdate( projectRef,{ teamLeader, $push: { teamMembers: {$each: [{ devRef:  teamMembers.devRef }] } }},
+            async(err,dt)=>{
             if(err) return res.status(400).send({type:'error', message: err.message});
             return res.status(200).send({type:'success',message:'Team is added to the project'});
         })
--- controller/pm/update.team.controller.js
+++ controller/pm/update.team.controller.js
@@ -2,7 +2,7 @@ const Team = require('../../models/projectTeamModel');
 
 module.exports = async(req,res)=>{
     try {
-        Team.findOneAndUpdate({projectRef: req.body.projectRef},{$set: req.body},
+        Team.findOneAndUpdate({projectRef: req.body.projectRef},{ $pull: { teamMembers: { devRef:  req.body.devRef }}},
             {runValidators:true,upsert:true,new:true},async(err,dt)=>{
             if(err) return res.status(400).send({type:'error', message: err.message});
             return res.status(200).send({type:'success',message:'Team is updated to the project'});
@@ -13,11 +13,3 @@ module.exports = async(req,res)=>{
         res.status(500).send({type: 'error', message: 'Error while connecting to the server!'});
     }
 }
- //  team =  await Team.find({projectRef: req.body.projectRef}).populate('teamMembers','name');
-    //  if(!team) res.status(500).send({type: 'warn', message: 'No Tasks'});
-    //  else{
-    //      var teamMem
-    //     team.forEach(element => {
-    //         teamMem = element.teamMembers;
-    //     });
-    // }
