6ff0c905474997211b0de5e32392cbaf4ca9066f
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-16 11:05:24 +0530

    first commit

--- controller/pm/update.team.controller.js
+++ controller/pm/update.team.controller.js
@@ -1,11 +1,28 @@
 const Team = require('../../models/projectTeamModel');
 
+// module.exports = async(req,res)=>{
+//     try {
+//         Team.findOneAndUpdate({projectRef: req.body.projectRef},{ $pull: { teamMembers: { devRef:  req.body.devRef }}},
+//             {runValidators:true,upsert:true,new:true},async(err,dt)=>{
+//             if(err) return res.status(400).send({type:'error', message: err.message});
+//             return res.status(200).send({type:'success',message:'Team is updated to the project'});
+//         })
+        
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({type: 'error', message: 'Error while connecting to the server!'});
+//     }
+// }
+
 module.exports = async(req,res)=>{
+    const { projectRef, teamLeader, teamMembers} = req.body;
     try {
-        Team.findOneAndUpdate({projectRef: req.body.projectRef},{ $pull: { teamMembers: { devRef:  req.body.devRef }}},
-            {runValidators:true,upsert:true,new:true},async(err,dt)=>{
+        Team.findOneAndUpdate({projectRef: projectRef},{
+            teamLeader,
+            $addToSet: { teamMembers: { $each: [ {devRef: teamMembers.devRef}  ] } }
+        },async(err,dt)=>{
             if(err) return res.status(400).send({type:'error', message: err.message});
-            return res.status(200).send({type:'success',message:'Team is updated to the project'});
+            return res.status(200).send({type:'success',message:'Team is added to the project'});
         })
         
     } catch (error) {
