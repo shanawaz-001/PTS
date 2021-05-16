30dac062897b8cfc57d168be8c18d7f50b7d5c41
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-16 11:51:24 +0530

    first commit

---
+++ models/todoModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    employeeRef:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Employee',
+        unique: [true, 'Already exists'],
+        required: true
+    },
+    todos:[
+        {
+            note:{
+                type: String,
+                required:true
+            },
+            isDone:{
+                type:Boolean,
+            },
+            createdDate:{
+                type:Date,
+                required:true,
+                default: Date.now
+            }
+        }
+    ]
+},
+{
+    collection:'todo'
+}
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
