edcb9a462ef364130337e9b57b41535861a9b06f
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-17 10:47:06 +0530

    first commit

--- models/projectTaskModel.js
+++ models/projectTaskModel.js
@@ -35,7 +35,7 @@ const projectTaskSchema = new mongoose.Schema({
     credits:{
         type: String,
         validate:{
-            validator: value => ['1','2','3','4','5','6','7','8','9','10'].includes(value),
+            validator: value => ['1','2','3','4','5'].includes(value),
             message: 'Invalid Credits'
         },
         default: '1'
