0f466616e59b433ba33069f8ebb03bbcd63c4bf2
shanawaz-001 <mohammadshanawaz58@gmail.com>
2021-05-18 22:58:20 +0530

    first commit

--- controller/pm/update.team.controller.js
+++ controller/pm/update.team.controller.js
@@ -1,29 +1,15 @@
 const Team = require('../../models/projectTeamModel');
-const assignTask = require('../../models/assignTaskModel');
+
 module.exports = async(req,res)=>{
     try {
         const { projectRef, teamLeader, teamMembers} = req.body;
-        const teamOld = await Team.find({projectRef: projectRef}).select('teamMembers.devRef');
-        var teamOldMembers = teamOld.map(item => item.teamMembers)[0];
-        teamOldMembers = teamOldMembers.map(({devRef}) => devRef)
-        var teamNewMembers = teamMembers;
-        var test = teamOldMembers.filter(x => {
-            for(let newMember of teamNewMembers) {
-                return !(newMember.includes(x) || newMember == x)
-            }
-        });
         await Team.findOneAndUpdate({projectRef: projectRef},{
             teamLeader,
             teamMembers
         },{upsert:true},async(err,dt)=>{
             if(err) return res.status(400).send({type:'error', message: err.message});
+            return res.status(200).send({type:'success',message:'Team is added to the project'});
         })
-        if(test){
-         await assignTask.findOneAndDelete({devRef: {$in: test}}).exec((e,d)=>{
-             if(e) return res.status(400).send({type:'error', message: e.message});
-             else return res.status(200).send({type:'success',message:'Team is added/updated to the project'});
-         })
-        }
         
     } catch (error) {
         console.error(error);
